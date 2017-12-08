975c21028dc8ad44efa170d33165c3b43bc94d6c
Michael Edenzon <edenzonmp@g.cofc.edu>
2017-12-08 16:14:49 -0500

    Initial Commit

---
+++ HapticButtons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HapticButtons
+//
+//  Created by Michael Edenzon on 12/8/17.
+//  Copyright © 2017 Michael Edenzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
